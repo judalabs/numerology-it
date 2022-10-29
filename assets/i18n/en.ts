8716dac4ae7017c3a0f15edba5891d91a24368bc
conceicao.rodrigo.j@gmail.com <conceicaorodrigoj@gmail.com>
2022-10-29 18:37:06 -0300

    Auto-generated commit [ci skip]

---
+++ assets/i18n/en.ts
@@ -0,0 +1,9 @@
+import { TranslationObject } from "./pt";
+
+const TRANSLATIONS: TranslationObject = {
+    "ANALYSIS": {
+        "DESTINY": "GHI"
+    }
+}
+
+export default TRANSLATIONS;
---
+++ assets/i18n/pt.ts
@@ -0,0 +1,9 @@
+const TRANSLATIONS = {
+    "ANALYSIS": {
+        "DESTINY": "JKL"
+    },
+}
+
+export type TranslationObject = typeof TRANSLATIONS;
+
+export default TRANSLATIONS;
